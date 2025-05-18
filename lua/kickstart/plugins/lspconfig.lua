92dcb63aa9a30b89a22d43d4fc41f3db68498ab7
sins621 <sinsmailza@gmail.com>
2025-05-18 18:03:30 +0200

    update: remove unneeded gdscript lsp config

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -270,14 +270,6 @@ return {
           end,
         },
       }
-      -- local gdscript_config = {
-      --   capabilities = capabilities,
-      --   settings = {},
-      -- }
-      -- if vim.fn.has 'win32' == 1 then
-      --   gdscript_config['cmd'] = { 'ncat', 'localhost', os.getenv 'GDScript_Port' or '6005' }
-      -- end
-      -- require('lspconfig').gdscript.setup(gdscript_config)
     end,
   },
 }
