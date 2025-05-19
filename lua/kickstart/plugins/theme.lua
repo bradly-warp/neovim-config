445e82b1dc415b7cb9dc4270ea080af87a43dcc3
sins621 <sinsmailza@gmail.com>
2025-05-19 09:12:11 +0200

    update: theme.lua

--- lua/kickstart/plugins/theme.lua
+++ lua/kickstart/plugins/theme.lua
@@ -1,27 +1,10 @@
 return {
   {
-    'kepano/flexoki-neovim',
-    name = 'flexoki',
+    'AlexvZyl/nordic.nvim',
+    name = 'nordic',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'flexoki-dark'
-      vim.cmd [[
-        " Transparent background
-        hi Normal guibg=NONE ctermbg=NONE
-        hi NormalNC guibg=NONE ctermbg=NONE
-        hi EndOfBuffer guifg=bg guibg=NONE
-        hi FloatBorder guibg=NONE ctermbg=NONE
-        hi NormalFloat guibg=NONE ctermbg=NONE
-        hi MsgArea guibg=NONE ctermbg=NONE
-        hi MsgSeparator guibg=NONE ctermbg=NONE
-        hi StatusLine guibg=NONE ctermbg=NONE
-
-        " Keep contrast where needed
-        hi Pmenu guibg=NONE ctermbg=NONE
-        hi PmenuSel guibg=#3a3b4c ctermbg=NONE
-        hi PmenuSbar guibg=#3a3b4c ctermbg=NONE
-        hi PmenuThumb guibg=#5a5b6c ctermbg=NONE
-      ]]
+      require('nordic').load()
     end,
   },
 }
